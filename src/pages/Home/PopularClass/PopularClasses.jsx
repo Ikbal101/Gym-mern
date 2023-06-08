21b8629950051806589ddc27d1d5705adcd32205
webabdulla <webabdullah7410@gmail.com>
2023-06-08 06:17:46 +0600

    popular class added

--- src/pages/Home/PopularClass/PopularClasses.jsx
+++ src/pages/Home/PopularClass/PopularClasses.jsx
@@ -44,7 +44,7 @@ const PopularClasses = () => {
 
   return (
     <div className='mt-24 mb-10'>
-      <h2 className='popular-classes'>Popular Classes <hr className='w-11 h-1  mx-auto bg-red-600' /></h2>
+      <h2 className='popular-classes'>Popular Classes  <hr className='w-11 h-1  mx-auto bg-red-600' /></h2>
       <div className="class-container">
         {sortedClasses.slice(0, 6).map((classItem) => (
           <div key={classItem.title} className="class-card">
